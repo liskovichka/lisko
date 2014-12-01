c0b755886527e6d2cbc2f4f468ec3e5d3f204064
john <lisko4ich@gmail.com>
2014-12-01 14:14:59 +0200

    trying to change 2

--- src/main/java/myMain.java
+++ src/main/java/myMain.java
@@ -4,6 +4,10 @@
 public class myMain {
     public static void main (String[] args){
         System.out.println("dsfsdfsdfsdf");
+        System.out.println("dsfsdfsdfsdf");
+        System.out.println("dsfsdfsdfsdf");
+        System.out.println("dsfsdfsdfsdf");
+        System.out.println("dsfsdfsdfsdf");
 
     }
 }
